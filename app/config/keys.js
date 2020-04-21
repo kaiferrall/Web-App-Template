477879624942a81bc009f7cfa7e8c914de78c368
Kai Ferrall <kaiferrall@gmail.com>
2020-04-20 23:50:54 -0400

    Included config file for clarity

---
+++ app/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    databaseURI:"database string uri",
+    jwtSecret: "dasdkjasbdjsadbjasdba",
+    links: {
+      verifyEmail:
+        "http://localhost:5000/public/user/authentication-services/confirm-email/",
+      resetPasswordLink:
+        "http://localhost:5000/user/authentication-services/recover-password/"
+    },
+  };
